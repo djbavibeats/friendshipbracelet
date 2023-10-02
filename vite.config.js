bd48f1f2581bd21587e57f27440288aeb25fb1fb
djbavibeats <justin@voltcreative.com>
2023-10-02 15:03:57 -0400

    Updates

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: '/src/main.jsx',
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000'
-      }
-    }
-  }
 })
