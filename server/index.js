6ee69260d198c63c8c59fc398452b99dd0bf212e
djbavibeats <justin@voltcreative.com>
2023-10-02 15:09:39 -0400

    Tryingg

--- server/index.js
+++ server/index.js
@@ -6,68 +6,63 @@ import cors from 'cors'
 
 const app = express()
 ViteExpress.config({ mode: 'production' })
-const PORT = 5000
-const server = createServer(app)
-const io = new Server(server, {
-    cors: {
-        origin: [ 'http://localhost:5173' ]
-    }
-})
-app.use(cors())
 
-io.use((socket, next) => {
-    const username = socket.handshake.auth.username
-    if (!username) {
-        return next(new Error('invalid username'))
-    }
-    socket.username = username
-    next()
-})
+app.get('/test', (_, res) => res.send('hello from the server!'))
 
-io.on('connection', (socket) => {
-    console.log(`connect: ${socket.id}`)
-
-    const users = []
-    for (let [ id, socket ] of io.of('/').sockets) {
-        users.push({
-            userId: id,
-            username: socket.username
-        })
-    }
-    socket.emit('users', users)
-    
-
-    socket.broadcast.emit('user connected', {
-        userId: socket.id,
-        username: socket.username,
-    })
-
-    // Uncomment to listen to any event (custom named!)
-    // socket.onAny((event, ...args) => {
-    //     console.log("any " + event)
-    // })
-
-    socket.on('private message', ({ message, to }) => {
-        socket.to(to).emit('private message', {
-            message,
-            from: socket.id
-        })
-    })
-
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('user disconnected', socket.id)
-    })
-})
-
-app.get('/api/v1/hello', (req, res) => {
-    res.json({
-        message: 'Hi!!!'
-    })
-})
-
-ViteExpress.listen(server, 5000, () => {
+ViteExpress.listen(app, 5000, () => {
     console.log('server is listening')
 })
+// const server = createServer(app)
+// const io = new Server(server, {
+//     cors: {
+//         origin: [ 'http://localhost:5173' ]
+//     }
+// })
+// app.use(cors())
+
+// io.use((socket, next) => {
+//     const username = socket.handshake.auth.username
+//     if (!username) {
+//         return next(new Error('invalid username'))
+//     }
+//     socket.username = username
+//     next()
+// })
+
+// io.on('connection', (socket) => {
+//     console.log(`connect: ${socket.id}`)
+
+//     const users = []
+//     for (let [ id, socket ] of io.of('/').sockets) {
+//         users.push({
+//             userId: id,
+//             username: socket.username
+//         })
+//     }
+//     socket.emit('users', users)
+    
+
+//     socket.broadcast.emit('user connected', {
+//         userId: socket.id,
+//         username: socket.username,
+//     })
+
+//     // Uncomment to listen to any event (custom named!)
+//     // socket.onAny((event, ...args) => {
+//     //     console.log("any " + event)
+//     // })
+
+//     socket.on('private message', ({ message, to }) => {
+//         socket.to(to).emit('private message', {
+//             message,
+//             from: socket.id
+//         })
+//     })
+
+//     socket.on('disconnect', () => {
+//         socket.broadcast.emit('user disconnected', socket.id)
+//     })
+// })
 
 // server.listen(PORT, () => {
 //     console.log('listening... on PORT: ' + PORT)
